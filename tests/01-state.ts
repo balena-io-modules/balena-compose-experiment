c82584fb05c81a755c0a5df1fea967d02ecf23e0
Stathis <emoraitidis@gmail.com>
2021-03-23 16:33:47 +0000

    First running test

---
+++ tests/01-state.ts
@@ -0,0 +1,35 @@
+import { expect } from './chai';
+import { Composer, ComposerOptions, ComposerState } from '../lib/index';
+import { testWithData } from './mocked-dockerode';
+// import { Volume } from 'dockerode'
+
+describe('TypeScript library skeleton:', function () {
+	it('should be able to call myFunc on a new instance', async function () {
+		const options: ComposerOptions = {
+			uuid: '1',
+			apiKey: '123',
+			apiEndpoint: 'url',
+			deltaEndpoint: 'endpoint',
+			delta: true,
+			deltaRequestTimeout: 10,
+			deltaApplyTimeout: 10,
+			deltaRetryCount: 10,
+			deltaRetryInterval: 10,
+			deltaVersion: 10,
+		};
+
+		const instance = new Composer(123, options);
+
+		const expected: ComposerState = {
+			status: 'idle',
+			app: 123,
+			volumes: {},
+			networks: {},
+			services: {},
+		};
+
+		testWithData({ containers: [] }, async () => {
+			await expect(instance.state()).to.eventually.become(expected);
+		});
+	});
+});
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,13 +1,75 @@
-import { expect } from './chai';
-import { ImplementClass } from '../lib/index';
+// import { expect } from './chai';
+// import { Composer, ComposerOptions } from '../lib/index';
+
+// export type ComposerState = {
+// 	status: ComposerRuntime['status'];
+// 	app: number; // app-id for now
+// 	release?: string; // release-commit or version
+// 	services: {
+// 		[key: string]: Service;
+// 	};
+// 	volumes: {
+// 		[key: string]: Volume;
+// 	};
+// 	networks: {
+// 		[key: string]: Network;
+// 	};
+// };
 
 describe('TypeScript library skeleton:', function () {
 	it('should be able to call myFunc on a new instance', async function () {
-		const instance = new ImplementClass();
-		expect(instance.myFunc).to.be.a('function');
-
-		await expect(instance.myFunc()).to.eventually.become(
-			`I need implementing! 1`,
-		);
+		// const options: ComposerOptions = {
+		// 	uuid: '1',
+		// 	apiKey: '123',
+		// 	apiEndpoint: 'url',
+		// 	deltaEndpoint: 'endpoint',
+		// 	delta: true,
+		// 	deltaRequestTimeout: 10,
+		// 	deltaApplyTimeout: 10,
+		// 	deltaRetryCount: 10,
+		// 	deltaRetryInterval: 10,
+		// 	deltaVersion: 10,
+		// };
+		// const instance = new Composer(123, options);
+		// const expected = {
+		// 	status: 'idle',
+		// 	app: '123',
+		// 	volumes: [],
+		// 	networks: []
+		// }
+		// // expect(instance.state()).to.be.a(expected);
+		// await expect(instance.state()).to.eventually.become(expected);
+		// await expect(instance.myFunc()).to.eventually.become(
+		// 	`I need implementing! 1`,
+		// );
 	});
 });
+
+/* Update Scenarios TODO:
+
+download-then-kill strategy
+
+Current State					Target State
+------------					------------
+no containers					no containers
+no containers					1 container
+no containers					2 containers
+1 container						no containers
+2 container						no containers
+1 container						same container
+1 container						1 different container
+2 container						2 different containers
+2 container						1 different container and one same conatiner
+
+kill-then-download strategy
+
+Current State					Target State
+------------					------------
+no containers					1 container
+1 container						no containers
+1 container						same container
+1 container						1 different container
+2 container						2 different containers
+2 container						1 different container and one same conatiner
+
+*/
