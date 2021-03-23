d6aac50efe904148a165d2e375a81952d2efd1b1
Robert Günzler <r@gnzler.io>
2021-03-23 16:56:32 +0100

    Add command line interface
    intended primarily for testing purposes
    
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,70 @@
 import { Composer } from '../lib';
 
-// Here goes the CLI codebase
-console.log(Composer);
+async function up(composer: Composer, targetStatePath: string): Promise<void> {
+	const target_state = require(targetStatePath);
+	const initial_state = await composer.state();
+
+	console.debug("initial state:", initial_state);
+	console.debug("target state:", target_state);
+
+	await composer.update(target_state);
+}
+
+async function down(_: Composer): Promise<void> {
+	throw new Error("not implemented");
+}
+
+async function main(): Promise<void> {
+	// TODO probably only works when you do `node index.js up`
+	const argv = process.argv.slice(2);
+	const command = argv[0];
+	const composer = new Composer(1, {
+		uuid: '1',
+		deviceApiKey: '123',
+		apiEndpoint: 'url',
+		deltaEndpoint: 'endpoint',
+		delta: true,
+		deltaRequestTimeout: 10,
+		deltaApplyTimeout: 10,
+		deltaRetryCount: 10,
+		deltaRetryInterval: 10,
+		deltaVersion: 10,
+	});
+
+	try {
+		switch (command) {
+		case "help":
+		case "-h":
+		case "--help":
+			console.log("usage: balena-compose [command]");
+			break;
+
+		case "version":
+		case "--version":
+			console.log("balena-compose 0.0.0");
+			break;
+
+		case "up":
+			await up(composer, argv[1]);
+			break;
+
+		case "down":
+			await down(composer);
+			break;
+
+		default:
+			throw new Error(`subcommand not known: ${argv[0]}`);
+		}
+	} catch (e) {
+		throw e
+	}
+}
+
+main()
+	.then(() => {
+		process.exit(0);
+	})
+	.catch(e => {
+		console.error(e);
+		process.exit(1);
+	});
