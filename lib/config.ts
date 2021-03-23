e06ac92b88943567186edd95b08e1886e9c2c8f9
Felipe Lalanne <felipe@balena.io>
2021-03-23 11:21:37 -0300

    Add default configuration options
    Co-authored-by: Stathis Moraitidis <stathismor@users.noreply.github.com>
    Co-authored-by: toochevere <toochevere@users.noreply.github.com>

--- lib/config.ts
+++ lib/config.ts
@@ -3,19 +3,28 @@ import * as packageJson from '../package.json';
 const version = packageJson.version;
 const config = new Map();
 
+// Set defaults
+config.set('apiEndpoint', 'https://api.balena-cloud.com');
+config.set('appUpdatePollInterval', 900000);
+config.set('deltaEndpoint', 'https://delta.balena-cloud.com');
+config.set('deviceType', 'raspberrypi4-64');
+config.set('delta', true);
+config.set('deltaRequestTimeout', 30000);
+config.set('deltaRetryCount', 30);
+config.set('deltaRetryInterval', 10000);
+config.set('deltaVersion', 3);
+
 // TODO: fetchOptions should be obtained from a combination of configs
 config.set('fetchOptions', {
-	uuid: 'deafbeef',
-	currentApiKey: 'abc',
-	apiEndpoint: 'https://api.balena-cloud.com',
-	deltaEndpoint: 'https://delta.balena-cloud.com',
-	delta: true,
-	deltaRequestTimeout: 30000,
-	deltaApplyTimeout: 0,
-	deltaRetryCount: 30,
-	deltaRetryInterval: 10000,
-	deltaVersion: 3,
-	supervisorNetworkInterface: '', // TODO:
+	uuid: config.get('uuid'),
+	deviceApiKey: config.get('deviceApiKey'),
+	apiEndpoint: config.get('apiEndpoint'),
+	deltaEndpoint: config.get('deltaEndpoint'),
+	delta: config.get('delta'),
+	deltaRequestTimeout: config.get('deltaRequestTimeout'),
+	deltaRetryCount: config.get('deltaRetryCount'),
+	deltaRetryInterval: config.get('deltaRetryInterval'),
+	deltaVersion: config.get('deltaVersion'),
 });
 
 config.set('osVersion', '2.72');
