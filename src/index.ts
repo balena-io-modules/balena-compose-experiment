e390f72450ce434bdc434cc7d7184df7f7c43104
Robert Günzler <r@gnzler.io>
2021-03-25 14:26:43 +0100

    cli: use yargs
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,83 @@
-import { Composer, ComposerTarget } from '../lib';
+import { Composer } from '../lib';
 
-async function up(composer: Composer, targetStatePath: string): Promise<void> {
-	const target_state = require(targetStatePath);
-	const initial_state = await composer.state();
+import { getSdk } from 'balena-sdk';
+import * as yargs from 'yargs';
+import * as fs from 'fs';
 
-	console.debug("initial state:", initial_state);
-	console.debug("target state:", target_state);
+async function up(args: any): Promise<void> {
+	let targetState;
+	if (args.file) {
+		const data  = fs.readFileSync(args.file);
+		targetState = JSON.parse(data);
 
-	await composer.update(target_state);
+	} else {
+		const balena = getSdk({ apiUrl: args.apiUrl ?? 'https://api.balena-cloud.com' });
+		await balena.auth.loginWithToken(args.apiKey);
+		targetState = (
+			await balena.models.device.getSupervisorTargetState(
+				args.uuid,
+			)
+		).local.apps[args.appid];
+	}
+
+	const composer = new Composer(args.appid, {
+		uuid: args.uuid,
+		deviceApiKey: args.apiKey,
+		delta: args.delta,
+		apiEndpoint: args.apiUrl ?? undefined,
+		deltaEndpoint: args.deltaUrl ?? undefined,
+	});
+
+	console.debug("initial state:", await composer.state());
+	console.debug("target state:", targetState);
+
+	await composer.update(targetState);
 }
 
-async function down(composer: Composer): Promise<void> {
-	const target_state: ComposerTarget = {
-		name: 'test1',
-		services: {},
-		volumes: {},
-		networks: {},
-	};
-	await composer.update(target_state);
+async function down(args: any): Promise<void> {
+	const composer = new Composer(args.appid, {
+		uuid: args.uuid,
+		deviceApiKey: args.apiKey,
+		delta: args.delta,
+	});
+	await composer.update({name: "", services: {}, volumes: {}, networks: {}});
 }
 
 async function main(): Promise<void> {
-	// TODO probably only works when you do `node index.js up`
-	const argv = process.argv.slice(2);
-	const command = argv[0];
-	const composer = new Composer(1, {
-		uuid: '1',
-		deviceApiKey: '123',
-		apiEndpoint: 'url',
-		deltaEndpoint: 'endpoint',
-		delta: true,
-		deltaRequestTimeout: 10,
-		deltaApplyTimeout: 10,
-		deltaRetryCount: 10,
-		deltaRetryInterval: 10,
-		deltaVersion: 10,
-	});
-
 	try {
-		switch (command) {
-		case "help":
-		case "-h":
-		case "--help":
-			console.log("usage: balena-compose [command]");
-			break;
+		await yargs(process.argv.slice(2))
+			.usage('$0 [global args] [command] [args]')
+			.option('file', {
+				alias: 'f',
+				type: 'string',
+				description: 'target state to apply'
+			})
+			.option('appid', {
+				alias: 'a',
+				type: 'number',
+			})
+			.option('uuid', {
+				alias: 'u',
+				type: 'string',
+			})
+			.option('api-key', {
+				type: 'string',
+			})
+			.option('api-url', {
+				type: 'string',
+			})
+			.option('api-url', {
+				type: 'string',
+			})
+			.option('delta-url', {
+				type: 'string',
+			})
+			.command('up', 'apply target state', {}, up)
+			.command('down', 'reset to empty state', {}, down)
+			.demandCommand()
+			.help()
+			.argv;
 
-		case "version":
-		case "--version":
-			console.log("balena-compose 0.0.0");
-			break;
-
-		case "up":
-			await up(composer, argv[1]);
-			break;
-
-		case "down":
-			await down(composer);
-			break;
-
-		default:
-			throw new Error(`subcommand not known: ${argv[0]}`);
-		}
 	} catch (e) {
 		throw e
 	}
